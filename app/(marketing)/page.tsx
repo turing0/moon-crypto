f01b8f145c4f7c9eadce273113f74fc353e5d802
turing0 <turing175@gmail.com>
2024-07-08 21:59:46 +0800

    add exchanges

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,16 +6,19 @@ import InfoLanding from "@/components/sections/info-landing";
 import Powered from "@/components/sections/powered";
 import PreviewLanding from "@/components/sections/preview-landing";
 import Testimonials from "@/components/sections/testimonials";
+import Exchanges from "@/components/sections/exchanges";
 
 export default function IndexPage() {
   return (
     <>
       <HeroLanding />
       <PreviewLanding />
-      <Powered />
+      {/* <Powered /> */}
+      <Exchanges />
       <BentoGrid />
-      <InfoLanding data={infos[0]} reverse={true} />
+      {/* <InfoLanding data={infos[0]} reverse={true} /> */}
       {/* <InfoLanding data={infos[1]} /> */}
+      <InfoLanding data={infos[1]} />
       <Features />
       <Testimonials />
     </>
