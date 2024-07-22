82478721f69291b1bcdb01d3c7c5fedde328a867
turing0 <turing175@gmail.com>
2024-07-22 20:42:12 +0800

    update loading

---
+++ app/(protected)/copy-trading/manage/loading.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { DashboardHeader } from "@/components/dashboard/header";
+import { DashboardShell } from "@/components/dashboard/shell";
+
+export default function CTMLoading() {
+  return (
+    <DashboardShell>
+      <DashboardHeader
+        heading="Manage Copy-Trading"
+        // text=""
+      />
+      <div className="divide-border-200 divide-y rounded-md border">
+        <Skeleton className="h-[400px] w-full rounded-lg" />
+      </div>
+    </DashboardShell>
+  );
+}
