4831e53efc1249000dc5d13f561d44a45d367569
turing0 <turing175@gmail.com>
2024-07-28 23:42:05 +0800

    update nav

--- components/layout/user-account-nav.tsx
+++ components/layout/user-account-nav.tsx
@@ -63,6 +63,17 @@ export function UserAccountNav() {
             </div>
 
             <ul role="list" className="mb-14 mt-1 w-full text-muted-foreground">
+              <li className="rounded-lg text-foreground hover:bg-muted">
+                <Link
+                  href="/dashboard"
+                  onClick={closeDrawer}
+                  className="flex w-full items-center gap-3 px-2.5 py-2"
+                >
+                  <LayoutDashboard className="size-4" />
+                  <p className="text-sm">Dashboard</p>
+                </Link>
+              </li>
+              
               {user.role === "ADMIN" ? (
                 <li className="rounded-lg text-foreground hover:bg-muted">
                   <Link
@@ -87,17 +98,6 @@ export function UserAccountNav() {
                 </li>
               )}
               {/* ) : null} */}
-
-              <li className="rounded-lg text-foreground hover:bg-muted">
-                <Link
-                  href="/dashboard"
-                  onClick={closeDrawer}
-                  className="flex w-full items-center gap-3 px-2.5 py-2"
-                >
-                  <LayoutDashboard className="size-4" />
-                  <p className="text-sm">Dashboard</p>
-                </Link>
-              </li>
 
               <li className="rounded-lg text-foreground hover:bg-muted">
                 <Link
