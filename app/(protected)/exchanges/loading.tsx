07d4725b2f863cf12fdaab598f3694fbef8ef59e
turing0 <turing175@gmail.com>
2024-07-28 11:21:58 +0800

    update build

--- app/(protected)/exchanges/loading.tsx
+++ app/(protected)/exchanges/loading.tsx
@@ -1,10 +1,9 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { DashboardHeader } from "@/components/dashboard/header";
-import { DashboardShell } from "@/components/dashboard/shell";
 
 export default function DashboardLoading() {
   return (
-    <DashboardShell>
+    <>
       <DashboardHeader
         heading="Exchanges"
         // text=""
@@ -12,6 +11,6 @@ export default function DashboardLoading() {
       <div className="divide-border-200 divide-y rounded-md border">
         <Skeleton className="h-[400px] w-full rounded-lg" />
       </div>
-    </DashboardShell>
+    </>
   );
 }
