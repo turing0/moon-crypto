2ff4c3d792e842651427007f2db782f2f347eba8
turing0 <turing175@gmail.com>
2024-07-28 18:44:32 +0800

    update layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -28,7 +28,8 @@ export default async function Dashboard({ children }: ProtectedLayoutProps) {
   }));
 
   return (
-    <div className="relative flex min-h-screen w-full">
+    // <div className="relative flex min-h-screen w-full">
+    <div className="relative flex min-h-screen flex-col lg:flex-row w-full">
       <DashboardSidebar links={filteredLinks} />
 
       <div className="flex flex-1 flex-col">
