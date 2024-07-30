e9700f5122e67d15aa5b30894178819c9bb27381
turing0 <turing175@gmail.com>
2024-07-30 11:38:04 +0800

    update delete

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -19,6 +19,7 @@ export const DELETE = auth(async (req) => {
       },
     });
   } catch (error) {
+    console.log("error:", error)
     return new Response("Internal server error", { status: 500 });
   }
 
