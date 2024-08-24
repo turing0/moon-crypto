44ada5f7d7af00dac7db5f0650eb5533dc3c6201
turing0 <turing175@gmail.com>
2024-08-24 22:09:35 +0800

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_APP_URL || 'https://mooncryp.to',
   generateRobotsTxt: false, // (optional)
+  changefreq: 'weekly',
   // ...other options
 }
