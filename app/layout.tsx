7a1a4ad5dc4d3138cae008c4a2cf21926b9a9a24
turing0 <turing175@gmail.com>
2025-03-10 21:39:44 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,14 @@ export const metadata = constructMetadata();
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        {/* <script 
+          defer 
+          src="https://cloud.umami.is/script.js" 
+          data-website-id=""
+          data-domains="mooncryp.to"
+        ></script> */}
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
