cf89766ed64705bdb96601d01efef2c297717790
unknown <turing175@gmail.com>
2025-04-01 13:43:04 +0800

    update login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,10 +3,16 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
 import { UserAuthForm } from "@/components/forms/user-auth-form";
 import { Icons } from "@/components/shared/icons";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { buttonVariants } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -15,51 +21,118 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      {/* <div className="mb-4">
-        <Alert className="!pl-14">
-          <Icons.warning />
-          <AlertTitle>Please use Google sign in.</AlertTitle>
-          <AlertDescription>
-            Currently not support email login.{" "}
-          </AlertDescription>
-        </Alert>
-      </div> */}
-      
-      <Link
-        href="/"
-        className={cn(
-          buttonVariants({ variant: "outline", size: "sm" }),
-          "absolute left-4 top-4 md:left-8 md:top-8",
-        )}
-      >
-        <>
-          <Icons.chevronLeft className="mr-2 size-4" />
-          Back
-        </>
-      </Link>
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <div className="flex flex-col space-y-2 text-center">
-          <Icons.logo className="mx-auto size-6" />
-          <h1 className="text-2xl font-semibold tracking-tight">
-            Welcome back
-          </h1>
-          <p className="text-sm text-muted-foreground">
-            Enter your email to sign in to your account
-          </p>
+    <>
+    {/* <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10"> */}
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "outline", size: "sm" }),
+            "absolute left-4 top-4 md:left-8 md:top-8",
+          )}
+        >
+          <>
+            <Icons.chevronLeft className="mr-2 size-4" />
+            Back
+          </>
+        </Link>
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Icons.logo className="mx-auto size-6" />
+          </div>
+          MoonCrypto
+        </a>
+
+        {/* <LoginForm /> */}
+
+        <div className={cn("flex flex-col gap-6")}>
+          <Card>
+            <CardHeader className="text-center">
+              <CardTitle className="text-xl">Welcome back</CardTitle>
+              <CardDescription>
+                Enter your email to sign in to your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="grid gap-6">
+
+                  <UserAuthForm />
+
+                  <div className="text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <a href="/register" className="underline underline-offset-4">
+                      Sign up
+                    </a>
+                  </div>
+                </div>
+            </CardContent>
+          </Card>
+          <div className="text-balance text-center text-xs text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link href="/terms" className="underline underline-offset-4 hover:text-primary">
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link href="/privacy" className="underline underline-offset-4 hover:text-primary">
+              Privacy Policy
+            </Link>
+            .
+          </div>
+
         </div>
-        <Suspense>
-          <UserAuthForm />
-        </Suspense>
-        <p className="px-8 text-center text-sm text-muted-foreground">
+
+        {/* <div className="container flex h-screen w-screen flex-col items-center justify-center">
+          <div className="mb-4">
+            <Alert className="!pl-14">
+              <Icons.warning />
+              <AlertTitle>Please use Google sign in.</AlertTitle>
+              <AlertDescription>
+                Currently not support email login.{" "}
+              </AlertDescription>
+            </Alert>
+          </div>
+          
           <Link
-            href="/register"
-            className="hover:text-brand underline underline-offset-4"
+            href="/"
+            className={cn(
+              buttonVariants({ variant: "outline", size: "sm" }),
+              "absolute left-4 top-4 md:left-8 md:top-8",
+            )}
           >
-            Don&apos;t have an account? Sign Up
+            <>
+              <Icons.chevronLeft className="mr-2 size-4" />
+              Back
+            </>
           </Link>
-        </p>
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <Icons.logo className="mx-auto size-6" />
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Welcome back
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email to sign in to your account
+              </p>
+            </div>
+            <Suspense>
+              <UserAuthForm />
+            </Suspense>
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              <Link
+                href="/register"
+                className="hover:text-brand underline underline-offset-4"
+              >
+                Don&apos;t have an account? Sign Up
+              </Link>
+            </p>
+          </div>
+        </div> */}
+
       </div>
     </div>
+
+
+    </>
   );
 }
